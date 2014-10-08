6ea3e535aacb18cb5ece556adc2d5f8511ad3f0b
BenWhitehead <benjamin.r.whitehead@gmail.com>
2014-10-08 16:43:58 -0700

    Update README.markdown to include usage information about new sub-commands.
    Also added usage/help into the script itself.

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
+function _rm {
+  rm -rf "$@" 2>/dev/null || true
+}
 RELEASE=`grep -1 -A 0 -B 0 '<version>' pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<version>//' | sed -e 's/<\/version>.*//'`
 
-RELEASE_ZIP_NAME=apache-storm-${RELEASE}.zip
+MIRROR=${MIRROR:-"http://www.gtlib.gatech.edu/pub"}
 
-MIRROR=http://www.gtlib.gatech.edu/pub
+function help {
+  cat <<USAGE
+Usage: bin/build-release.sh <storm.zip>
+  clean                   Attempts to clean working files and directories
+                            created when building.
+  mvnPackage              Runs the maven targets necessary to build the Storm
+                            Mesos framework.
+  prePackage <storm.zip>  Prepares the working directories to be able to
+                            package the Storm Mesos framework.
+  package                 Packages the Storm Mesos Framework.
+  downloadStormRelease    A utility function to download the Storm release zip
+                            for the targeted storm release.
+
+  ENV
+    MIRROR        Specify Apache Storm Mirror to download from
+                    Default: ${MIRROR}
+    RELEASE       The targeted release version of Storm
+                    Default: ${RELEASE}
+USAGE
+}; function --help { help ;}; function -h { help ;}
+
 function downloadStormRelease {
   wget --progress=dot:mega ${MIRROR}/apache/incubator/storm/apache-storm-${RELEASE}/apache-storm-${RELEASE}.zip
 }
 
 function clean {
-  rm -rf _release || true
-  rm -rf lib/ classes/ || true
-  rm -rf target || true
-  rm -f *mesos*.tgz || true
+  _rm _release
+  _rm lib/ classes/
+  _rm target
+  _rm *mesos*.tgz
 }
 
 function mvnPackage {
@@ -22,18 +45,18 @@ function mvnPackage {
   mvn dependency:copy-dependencies
 }
 
-function release {(
-  rm -rf _release
+function prePackage {(
+  _rm _release
   mkdir -p _release
   cp $1 _release/storm.zip
   cd _release
   unzip storm.zip
-  rm storm.zip
+  _rm storm.zip
   mv apache-storm* storm
 )}
 
 function package {(
-  rm _release/storm/*.jar 2> /dev/null || true
+  _rm _release/storm/*.jar
 
   # copies storm-mesos jar over
   cp target/*.jar _release/storm/lib/
@@ -52,7 +75,7 @@ function package {(
 function main {
   clean
   mvnPackage
-  release ${1:-${RELEASE_ZIP_NAME}}
+  prePackage ${1:-${RELEASE_ZIP_NAME}}
   package
 }
 
