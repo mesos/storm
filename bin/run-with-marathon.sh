46dc5aa7e64fe8f7ebad97b82c44dad8b76666fd
MrBar42 <mrbar42@users.noreply.github.com>
2016-03-17 14:01:22 +0200

    added additional opts variable
    allow adding options to command line of the ui and nimbus through environment variables
    - STORM_UI_OPTS
    - STORM_NIMBUS_OPTS

--- bin/run-with-marathon.sh
+++ bin/run-with-marathon.sh
@@ -7,12 +7,12 @@ echo "Running storm with MESOS_MASTER_ZK=${MESOS_MASTER_ZK}"
 
 function run_ui () {
   while [ true ] ; do
-    ./bin/storm ui -c ui.port=$PORT0 -c nimbus.thrift.port=$PORT1 -c storm.log.dir=$MESOS_SANDBOX/logs
+    ./bin/storm ui -c ui.port=$PORT0 -c nimbus.thrift.port=$PORT1 -c storm.log.dir=$MESOS_SANDBOX/logs ${STORM_UI_OPTS}
   done
 }
 
 function run_nimbus () {
-  ./bin/storm-mesos nimbus -c mesos.master.url=zk://${MESOS_MASTER_ZK}:2181/mesos -c storm.zookeeper.servers="[\"${MESOS_MASTER_ZK}\"]" -c nimbus.thrift.port=$PORT1  -c storm.log.dir=$MESOS_SANDBOX/logs
+  ./bin/storm-mesos nimbus -c mesos.master.url=zk://${MESOS_MASTER_ZK}:2181/mesos -c storm.zookeeper.servers="[\"${MESOS_MASTER_ZK}\"]" -c nimbus.thrift.port=$PORT1  -c storm.log.dir=$MESOS_SANDBOX/logs  ${STORM_NIMBUS_OPTS}
 }
 
 function run () {
