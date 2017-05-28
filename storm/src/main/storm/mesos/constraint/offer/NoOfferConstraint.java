7e4c2aaa8e8e55efbbcbfca0c5a05ff79001b50b
yuya <life.of.151a@gmail.com>
2017-05-28 16:30:14 +0900

    Create NoOfferConstraint class.

---
+++ storm/src/main/storm/mesos/constraint/offer/NoOfferConstraint.java
@@ -0,0 +1,23 @@
+/**
+ * NoOfferConstraint.java
+ *
+ * @since 2017/05/20
+ * <p/>
+ * Copyright 2017 fuji-151a
+ * All Rights Reserved.
+ */
+package storm.mesos.constraint.offer;
+
+import org.apache.mesos.Protos.Offer;
+import storm.mesos.constraint.Constraint;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public class NoOfferConstraint implements Constraint<Offer> {
+    @Override
+    public boolean isAccepted(final Offer target) {
+        return true;
+    }
+}
