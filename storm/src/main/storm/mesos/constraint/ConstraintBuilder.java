f99441dd24e6e61c0feb5115b3ddf405885ad587
yuya <life.of.151a@gmail.com>
2017-05-28 10:51:51 +0900

    Create ConstraintBuilder interface.

---
+++ storm/src/main/storm/mesos/constraint/ConstraintBuilder.java
@@ -0,0 +1,21 @@
+/**
+ * ConstraintBuilder.java
+ *
+ * @since 2017/05/20
+ * <p/>
+ * Copyright 2017 fuji-151a
+ * All Rights Reserved.
+ */
+package storm.mesos.constraint;
+
+import com.google.common.base.Optional;
+
+import java.util.Map;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public interface ConstraintBuilder<T> {
+    Optional<Constraint<T>> build(Map conf);
+}
