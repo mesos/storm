89548e55b80f8e97e8c7df9bbeba52724beb3062
yuya <life.of.151a@gmail.com>
2017-05-28 13:49:22 +0900

    Create AndConstraint class.

---
+++ storm/src/main/storm/mesos/constraint/AndConstraint.java
@@ -0,0 +1,27 @@
+/**
+ * AndConstraint.java
+ *
+ * @since 2017/05/20
+ * <p/>
+ * Copyright 2017 fuji-151a
+ * All Rights Reserved.
+ */
+package storm.mesos.constraint;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public class AndConstraint<T> implements Constraint<T> {
+    private final Constraint<T> one;
+    private final Constraint<T> two;
+
+    public AndConstraint(final Constraint<T> a, final Constraint<T> b) {
+        this.one = a;
+        this.two = b;
+    }
+    @Override
+    public boolean isAccepted(final T target) {
+        return one.isAccepted(target) && two.isAccepted(target);
+    }
+}
