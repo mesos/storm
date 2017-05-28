058b6fe8a82612d46f6af93c456cd6af0ec86cf8
yuya <life.of.151a@gmail.com>
2017-05-28 13:53:59 +0900

    Create AndConstraint Test.

---
+++ storm/src/test/storm/mesos/constraint/AndConstraintTest.java
@@ -0,0 +1,56 @@
+/**
+ * AndConstraintTest.java
+ *
+ * @since 2017/05/28
+ * <p>
+ * Copyright 2017 fuji-151a
+ * All Rights Reserved.
+ */
+package storm.mesos.constraint;
+
+import org.apache.mesos.Protos.Offer;
+import org.junit.Test;
+import storm.mesos.TestUtils;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public class AndConstraintTest {
+
+    @Test
+    public void trueAndTrueIsAccepted() throws Exception {
+        Constraint<Offer> a = new TestConstraint(true);
+        Constraint<Offer> b = new TestConstraint(true);
+        Constraint<Offer> offerConstraint = new AndConstraint<>(a, b);
+        assertTrue(offerConstraint.isAccepted(TestUtils.buildOffer()));
+    }
+
+    @Test
+    public void trueAndFalseIsNotAccepted() throws Exception {
+        Constraint<Offer> a = new TestConstraint(true);
+        Constraint<Offer> b = new TestConstraint(false);
+        Constraint<Offer> offerConstraint = new AndConstraint<>(a, b);
+        assertFalse(offerConstraint.isAccepted(TestUtils.buildOffer()));
+    }
+
+    @Test
+    public void falseAndTrueIsNotAccepted() throws Exception {
+        Constraint<Offer> a = new TestConstraint(false);
+        Constraint<Offer> b = new TestConstraint(true);
+        Constraint<Offer> offerConstraint = new AndConstraint<>(a, b);
+        assertFalse(offerConstraint.isAccepted(TestUtils.buildOffer()));
+    }
+
+    @Test
+    public void falseAndFalseIsNotAccepted() throws Exception {
+        Constraint<Offer> a = new TestConstraint(false);
+        Constraint<Offer> b = new TestConstraint(false);
+        Constraint<Offer> offerConstraint = new AndConstraint<>(a, b);
+        assertFalse(offerConstraint.isAccepted(TestUtils.buildOffer()));
+    }
+
+}
