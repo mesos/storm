aa8e17bfa4c2f082d3e6411631012e232a0061e0
yuya <life.of.151a@gmail.com>
2017-05-28 10:52:14 +0900

    Create MultiConstraintBuilder interface.

---
+++ storm/src/main/storm/mesos/constraint/MultiConstraintBuilder.java
@@ -0,0 +1,20 @@
+/**
+ * MultiConstraintBuilder.java
+ *
+ * @since 2017/05/20
+ * <p/>
+ * Copyright 2017 fuji-151a
+ * All Rights Reserved.
+ */
+package storm.mesos.constraint;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public interface MultiConstraintBuilder<T> {
+    List<Constraint<T>> build(Map conf);
+}
